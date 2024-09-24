ec37be4b29a53ea48fbba9e89b98544114e6a081
yessenia <yesseniaperez.n5@gmail.com>
2024-09-24 12:00:33 -0600

    Penguin class and name

---
+++ src/com/skilldistillery/spa/entities/Penguin.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.spa.entities;
+
+public class Penguin extends Animal {
+	
+//	methods
+	
+	public Penguin() {
+		super();
+	}
+	
+	public Penguin(String name) {
+		super(name);
+	}
+
+}
